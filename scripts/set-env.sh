1e5426ee931fb3b6031cc04f9dd6b5481442432c
Thiago Corá <thiago.cora@wises.com.br>
2026-05-29 01:20:13 -0300

    teste env

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "TESTE: $TESTE"
 if [ "$VERCEL_ENV" = "preview" ]; then
   echo "Preview: usando .env.development"
   cp .env.development .env.production
 fi
+
+echo "TESTE: $TESTE"
